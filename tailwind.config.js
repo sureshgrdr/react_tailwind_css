2a5e640ba09026f932a4ad14268713c857d03819
SureshNec <suresh.grdr@gmail.com>
2024-01-23 22:17:58 +0530

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        rubik: ["Rubik", "sans-serif"],
+        inter: ["inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};

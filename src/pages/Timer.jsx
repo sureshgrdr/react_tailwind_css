5fa769023cb0a60fec94beb5bd7b2d4bc105819c
SureshNec <suresh.grdr@gmail.com>
2024-01-29 22:41:09 +0530

    Modified Timer

--- src/pages/Timer.jsx
+++ src/pages/Timer.jsx
@@ -1,49 +1,93 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+const timings = [
+  { tname: "05.00 mins", ctime: 300 },
+  { tname: "10.00 mins", ctime: 600 },
+  { tname: "15.00 mins", ctime: 900 },
+  { tname: "20.00 mins", ctime: 1200 },
+];
 
 export default function Timer() {
   const [toggler, setToggler] = useState(true);
-  let time = 300;
-  let timerId;
+  const [time, setTime] = useState(0);
+  const [timerId, setTimerId] = useState(null);
+
+  useEffect(() => {
+    const formatTime = () => {
+      const hrs = String(Math.trunc(time / 3600)).padStart(2, "0");
+      const min = String(Math.trunc(time / 60)).padStart(2, "0");
+      const sec = String(Math.trunc(time % 60)).padStart(2, "0");
+      return `${hrs}:${min}:${sec}`;
+    };
+
+    document.querySelector(".disTime").textContent = formatTime();
+  }, [time]);
+
   const timer = () => {
-    if (timerId) clearInterval(timerId);
-    let distime = document.querySelector(".disTime");
-    function tick() {
-      const min = String(Math.trunc(time / 60)).padStart(2, 0);
-      const sec = String(Math.trunc(time % 60)).padStart(2, 0);
+    const watch = setInterval(() => {
+      setTime((prevTime) => {
+        if (prevTime === 0) {
+          clearInterval(watch);
+          setToggler(true);
+          return 0;
+        }
+        return prevTime - 1;
+      });
+    }, 1000);
 
-      distime.textContent = `${min}:${sec}`;
-      //   console.log(`${min}:${sec}`);
-      time--;
-    }
-    tick();
-    const watch = setInterval(tick, 1000);
-
-    return watch;
+    setTimerId(watch);
   };
-  if (timerId) clearInterval(timerId);
+
+  const startTimer = () => {
+    setToggler(false);
+    timer();
+  };
+
+  const stopTimer = () => {
+    setToggler(true);
+    clearInterval(timerId);
+  };
+
+  const selectTimer = (time) => {
+    setTime(time);
+    clearInterval(timerId);
+    setToggler(true);
+  };
+
+  const inputTimer = (e) => {
+    let inTime = e.target.value;
+    let [hrs, mns] = inTime.split(":");
+
+    console.log(inTime, parseInt(hrs) / 60, mns);
+  };
 
   return (
     <div>
-      <p className="disTime">00:00</p>
+      <p className="disTime text-xl text-orange-400 font-semibold"></p>
+      <ul>
+        {timings.map((item, i) => {
+          return (
+            <li key={i} onClick={() => selectTimer(item.ctime)}>
+              {item.tname}
+            </li>
+          );
+        })}
+      </ul>
+      <br />
+      <input
+        type="time"
+        id="appt"
+        name="appt"
+        onChange={(e) => inputTimer(e)}
+      ></input>
+
       <br />
       {toggler ? (
-        <button
-          className="btn startBtn"
-          onClick={() => {
-            setToggler(false);
-            timerId = timer();
-          }}
-        >
+        <button className="btn startBtn" onClick={startTimer}>
           ◷ Start
         </button>
       ) : (
-        <button
-          className="btn stopBtn"
-          onClick={() => {
-            setToggler(true);
-            clearInterval(timerId);
-          }}
-        >
+        <button className="btn stopBtn" onClick={stopTimer}>
           ⊗ Stop
         </button>
       )}
